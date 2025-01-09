9cbca5f3d2bad20f43aa4594d5f6fad7909d2e16
Satya <satya_2302@yahoo.com>
2025-01-09 11:50:26 +0530

    modified spaces

--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
+# test.py
 import numpy as np
 import joblib
 from sklearn.datasets import load_iris
@@ -11,7 +12,6 @@ loaded_model = joblib.load('iris_model.joblib')
 if __name__ == "__main__":
     # Example usage
     example_features = [5.1, 3.5, 1.4, 0.2]
-    example_features=np.array(example_features).reshape(1, -1)
+    example_features = np.array(example_features).reshape(1, -1)
     prediction = loaded_model.predict(example_features)
-    print(f"The predicted {example_features} is: {prediction}")
-
+    print(f"The predicted class for the given features {example_features.tolist()} is: {data.target_names[prediction[0]]}")
