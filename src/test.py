e38e0e3f52679599ca99b462993741f32beb199f
Satya <satya_2302@yahoo.com>
2025-01-10 16:27:56 +0530

    modified pytest

--- src/test.py
+++ src/test.py
@@ -15,4 +15,5 @@ if __name__ == "__main__":
     example_features = [5.1, 3.5, 1.4, 0.2]
     example_features = np.array(example_features).reshape(1, -1)
     prediction = loaded_model.predict(example_features)
+    assert data.target_names[prediction[0]] in ["setosa", "versicolor", "virginica"]
     print(f"The predicted is: {data.target_names[prediction[0]]}")
