3ed731714bb8ebfab119f0e7b1cb843b42364496
Satya <satya_2302@yahoo.com>
2025-01-25 21:36:36 +0530

    Fix build Issues

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import mlflow
 import joblib
-import mlflow.sklearn
 import numpy as np
 
 # Initialize the Flask application
@@ -9,15 +8,19 @@ app = Flask(__name__)
 
 # Load the trained model from MLflow
 # Replace <RUN_ID> with the actual MLflow run ID for the best model
-#model = mlflow.sklearn.load_model("mlruns/572425099079902757/5bc902453bba4db8ad936ab04a6b3119/artifacts/model_optimized")
+# model = mlflow.sklearn.load_model
+# ("mlruns/572425099079902757/5bc902453bba4db8ad936ab04a6b3119/artifacts/model_optimized")
 model = joblib.load('iris_model.joblib')
 
 @app.route('/predict', methods=['POST'])
+
+
 def predict():
     try:
         # Get the features from the POST request
         data = request.json
-        features = np.array(data['features']).reshape(1, -1)  # Reshape to match input format
+        features = np.array(data['features']).reshape(1, -1)  
+        # Reshape to match input format
         
         # Make prediction
         prediction = model.predict(features)
--- src/train.py
+++ src/train.py
@@ -1,5 +1,4 @@
 # hyperparameter_tuning_gridsearch.py
-import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
@@ -26,7 +25,8 @@ param_grid = {
 
 # Set up the GridSearchCV
 grid_search = GridSearchCV(
-    RandomForestClassifier(random_state=42), param_grid, cv=5, scoring="accuracy", n_jobs=-1
+    RandomForestClassifier(random_state=42), param_grid, 
+    cv=5, scoring="accuracy", n_jobs=-1
 )
 
 # Run the grid search
@@ -45,7 +45,8 @@ mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 # Create an input example
-input_example = X_test[0].reshape(1, -1)   # Taking the first sample from X_test
+input_example = X_test[0].reshape(1, -1)   
+# Taking the first sample from X_test
 
 
 # Log the final model with MLflow
@@ -53,11 +54,16 @@ mlflow.set_experiment("Random Forest Experiment")
 
 with mlflow.start_run():
     mlflow.log_params({
-        "param_grid": str(param_grid),            # Document the grid search space
-        "best_n_estimators": best_params["n_estimators"],  # Best value for n_estimators
-        "best_max_depth": best_params["max_depth"],        # Best value for max_depth
-        "best_min_samples_split": best_params["min_samples_split"],  # Best value for min_samples_split
-        "best_min_samples_leaf": best_params["min_samples_leaf"]     # Best value for min_samples_leaf
+        "param_grid": str(param_grid),            
+        # Document the grid search space
+        "best_n_estimators": best_params["n_estimators"], 
+        # Best value for n_estimators
+        "best_max_depth": best_params["max_depth"],        
+        # Best value for max_depth
+        "best_min_samples_split": best_params["min_samples_split"],  
+        # Best value for min_samples_split
+        "best_min_samples_leaf": best_params["min_samples_leaf"]     
+        # Best value for min_samples_leaf
     })
     mlflow.log_metric("mse", mse)
     mlflow.log_metric("r2", r2)
